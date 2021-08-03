cc85247430616ce48fb2300472a784f052f2f0d1
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-08-02 22:55:31 -0400

    Added create user route.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { route } = require('.');
 const { User } = require('../../models');
 
 //login route
@@ -25,11 +26,35 @@ router.post('/login', async (req, res) => {
             req.session.user_id = userData.id;
             req.session.logged_in = true;
 
-            res.status(200).json({message: 'Your are not logged in.'});
+            res.status(200).json({message: 'Your are now logged in.'});
         });
     } catch (err) {
         res.status(400).json(err);
     }
-})
+});
+
+//create user route
+router.post('/create', async (req, res) => {
+    try {
+        //check if user exists in DB
+        const userData = await User.findOne({
+            where: { username: req.body.username }
+        });
+        //if found, return error
+        if (userData) {
+            res.status(400).json({ message: 'Username already exists.  Please try logging in or select a new username.'})
+            return;
+        }
+        //otherwise create new user
+        const newUser = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.status(200).json({ message: 'User account created successfully. Please login.'});
+    } catch (err) {
+        res.status(400).json({ error: err })
+    };
+});
 
 module.exports = router;
