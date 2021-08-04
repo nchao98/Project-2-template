45a592671720eaefb41da9fdd5f25f374f62b48a
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-08-04 11:13:56 -0400

    Added GET login route to render login page.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
         const existingIdeas = await Idea.findAll();
         //map json into plain text to pass to handlebars
         const ideas = existingIdeas.map((idea) => idea.get({ plain: true }));
-
+        console.log(ideas);
         //render homepage.handlebars and pass in the ideas and logged_in status
         res.render('homepage', {
             ideas, 
@@ -17,6 +17,16 @@ router.get('/', async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
+})
+
+//login route - return login page
+router.get('/login', async(req,res) => {
+    try {
+        res.render('login');
+    } catch (err) {
+        res.status(400).json(err);
+    }
+
 })
 
 module.exports = router;
