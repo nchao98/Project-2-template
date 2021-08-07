ec4af114811289b5735426e1a35d0b19f5468bfb
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-08-07 11:24:54 -0400

    Fixed issues with logout: constrollers/api/userRoutes.js: added an else to handle when the user is not logged in public/js/logout.js: modified messages and pages returned to the user views/layouts/main.handlebars: replace a-tag with button-tag, remove href, and added type='submit'

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -62,6 +62,8 @@ router.post('/logout', (req, res) => {
         req.session.destroy(() => {
             res.status(204).end();
         });
+    } else {
+        res.status(404).end();
     };
 });
 
